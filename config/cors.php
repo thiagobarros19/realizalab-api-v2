4c10bb311652e7e6fcdae4cbce21e1d4d778a340
Thiago Barros <thiago@officecom.com.br>
2026-06-13 15:15:46 -0300

    fix: deploy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://realizalab.com.br')),
 
     'allowed_origins_patterns' => [],
 
